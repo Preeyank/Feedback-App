89a82c5b42b1358c954c75631851fcd7efad82b2
Priyank Bardolia <bardoliapriyank@gmail.com>
2022-04-17 15:46:01 -0700

    Adding the Entire Feedback app Project for which the Front-end is almost done. The backend part is still remaning which I will commit next.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
+import { useState } from "react";
+import FeedbackData from "./Data/FeedbackData";
+import FeedbackList from "./components/FeedbackList";
+import Card from "./components/shared/Card";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <FeedbackForm />
+      <FeedbackStats feedback={feedback} />
+      <div className="contained"></div>
+      <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+    </>
   );
 }
-
 export default App;
---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Card from "./shared/Card";
+import Button from "./shared/Button";
+import RatingSelect from "./RatingSelect";
+
+function FeedbackForm() {
+  const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+  const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage("Text must be atleast 10 characters");
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+    setText(e.target.value);
+  };
+  return (
+    <Card>
+      <form>
+        <h2> How would you rate our service?</h2>
+        <RatingSelect />
+        <div className="input-group">
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a Review"
+            value={text}
+          />
+          <Button isDisabled={btnDisabled} type="submit">
+            Send
+          </Button>
+        </div>
+        {message && <div className="message">{message}</div>}
+      </form>
+    </Card>
+  );
+}
+
+export default FeedbackForm;
